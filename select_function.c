59f0ca39b7415ba21d456a5aa39fb0914f9bfce4
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-19 07:28:31 -0700

    task

---
+++ select_function.c
@@ -0,0 +1,35 @@
+#include"monty.h"
+/**
+ * selectf - selrct the function required
+ * @s: array of strings
+ * @line_num: line number of the file
+ * @token: tok
+ */
+void selectf(stack_t **s, char *token, unsigned int line_num)
+{
+	int i = 0;
+	char *line;
+	instruction_t arr[] = {
+	{"push", pushs},
+	{"pall", palls},
+	{"pint", pints},
+	{"pop", pops}, 
+	{"nop", nops}
+	};
+	for (i = 0; i < 5; i++)
+	{
+		if (cmp(arr[i].opcode, token))
+		{
+			arr[i].f(s, line_num);
+			return;
+		}
+	}
+	line = to_st(line_num);
+	write(2, "L", 1);
+	write(2, line, _strlen(line));
+	write(2, ": unknown instruction ", 22);
+	write(2, token, _strlen(token));
+	write(2, "\n", 1);
+	free(line);
+	exit(EXIT_FAILURE);
+}
