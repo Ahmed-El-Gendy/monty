6a2dd1d5a627aad7e92c08119c4d71d8813a46fb
Ramyrashad127 <ramy41599@gmail.com>
2023-10-19 09:35:05 -0700

    new

---
+++ sub.c
@@ -0,0 +1,24 @@
+#include"monty.h"
+/**
+ * sub - sub
+ * @stack: top
+ * @line_num: line
+ */
+void sub(stack_t **stack, unsigned int line_num)
+{
+        char *st = to_st(line_num);
+        stack_t *temp2;
+
+        if (len_stack(stack) < 2)
+        {
+                err("L"), err(st), err(": can't sub, stack too short\n");
+                free(st);
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
+        free(st);
+        temp2 = (*stack)->next;
+        temp2->n = (*stack)->n - temp2->n;
+        free(*stack);
+        *stack = temp2;
+}
