8a43dfb67185bddd189d3d22536c83127c4c4e9b
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-19 08:40:15 -0700

    task

--- function.c
+++ function.c
@@ -41,3 +41,26 @@ void swap(stack_t **stack, unsigned int line_num)
 	(*stack)->next = temp;
 	*stack = temp2;
 }
+/**
+ * add - add 2 elements
+ * @stack: head
+ * @line_num: int
+ */
+void add(stack_t **stack, unsigned int line_num)
+{
+	char *st = to_st(line_num);
+	stack_t *temp, *temp2;
+
+	if (len_stack(stack) < 2)
+	{
+		err("L"), err(st), err(": can't add, stack too short\n");
+		free(st);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	free(st);
+	temp2 = (*stack)->next;
+	temp2->n += (*stack)->n;
+	free(*stack);
+	*stack = temp2;
+}
