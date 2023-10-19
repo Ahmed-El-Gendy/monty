f92a45e26439afa979111859160fa9022d356d3a
Ahmed-El-Gendy <elgendyahmed753@gmail.com>
2023-10-19 10:10:17 -0700

    task

--- mod.c
+++ mod.c
@@ -6,16 +6,17 @@
  */
 void mods(stack_t **stack, unsigned int line_num)
 {
-        char *st = to_st(line_num);
-        stack_t *temp2;
+	char *st = to_st(line_num);
+	stack_t *temp2;
 
-        if (len_stack(stack) < 2)
-        {
-                err("L"), err(st), err(": can't mod, stack too short\n");
-                free(st);
-                free_stack(*stack);
-                exit(EXIT_FAILURE);
-        }
+	if (len_stack(stack) < 2)
+	{
+		err("L"), err(st), err(": can't mod, stack too short\n");
+		free(st);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+
+	}
 	if ((*stack)->n == 0)
 	{
 		err("L"), err(st), err(": division by zero");
@@ -23,10 +24,9 @@ void mods(stack_t **stack, unsigned int line_num)
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-        free(st);
-        temp2 = (*stack)->next;
-        temp2->n = temp2->n % (*stack)->n;
-        free(*stack);
-        *stack = temp2;
+	free(st);
+	temp2 = (*stack)->next;
+	temp2->n %= (*stack)->n;
+	free(*stack);
+	*stack = temp2;
 }
-
